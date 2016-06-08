2419c0287e921620861c7dff0ce2a4d1db104560
Dan Stineback <dstineback@gmail.com>
2016-06-08 16:12:03 -0700

    fixed merge

---
+++ test/delete-test.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const chai = require('chai');
+const expect = chai.expect;
+
+const fs = require('fs-extra');
+
+const del = require(__dirname + '/../lib/vorpal-delete');
+
+describe('should properly handle the delete functionality for plops', () => {
+  it('should recognize the delete function', () => {
+    expect(del).to.eql.function;
+  });
+
+  it('should fully remove the template path', () => {
+    let template_path = process.env.HOME + '/.config/plop/';
+    fs.remove(template_path, (err)=>{
+      if (err) return new Error('Deletion Failure');
+    });
+    expect(template_path).to.eql.null;
+  });
+});
