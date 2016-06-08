0e0243442aaf4d52954f0c6c6c6c5c71f8f88cd8
Stephen Schroeder <schrode50@gmail.com>
2016-06-08 14:39:52 -0700

    3 tests done

---
+++ test/use-test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const chai = require('chai');
+// const chaiHTTP = require('chai-http');
+// chai.use(chaiHTTP);
+// const GIT = require('nodegit');
+const expect = chai.expect;
+
+const install = require(__dirname + '/../lib/vorpal-install');
+
+describe('install unit test', () => {
+  it('should validate the install function of plopjs', () => {
+    expect(install).to.be.function;
+  });
+
+  it('should properly rename the input to filename if not customized as second arg', () => {
+    let template = 'https://github.com/PLOPdotJS/plopjs.git';
+    let default_name = template.split('/').slice(-1).join().slice(0, -4);
+    expect(default_name).to.eql('plopjs');
+  });
+
+  it('should have the incorrect name to NOT EQUAL the default', () => {
+    let template = 'https://github.com/PLOPdotJS/plopjs.git';
+    let default_name = template.split('/').slice(-1).join().slice(0, -4);
+    expect(default_name).to.not.eql('plops');
+  });
+});
