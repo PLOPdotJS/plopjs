7565aae6e5a8451a3c74ac2ac9223a5fb40e5405
Stephen Schroeder <schrode50@gmail.com>
2016-06-08 12:59:57 -0700

    need to pull

--- test/test.js
+++ test/test.js
@@ -2,11 +2,12 @@
 
 const chai = require('chai');
 const expect = chai.expect;
-
+const should = require('chai').should();
 const install = require(__dirname + '/../lib/vorpal-install');
 
 describe('install unit test', () => {
   it('should validate the install function of plopjs', () => {
     expect(install).to.be.function;
+    expect(install).to.have.property('template');
   });
 });
