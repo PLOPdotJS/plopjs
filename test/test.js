1954819bbd9245ba0d685529067594b654b3315e
Dan Stineback <dstineback@gmail.com>
2016-06-08 10:31:28 -0700

    fixed merge

--- test/test.js
+++ test/test.js
@@ -4,18 +4,26 @@ const chai = require('chai');
 const chaiHTTP = require('chai-http');
 chai.use(chaiHTTP);
 const expect = chai.expect;
-// const request = require('chai').request;
+const should = require('chai').should();
+const request = require('chai').request;
 //
-// const ourApp = require(__dirname + '/../app/app');
+const ourApp = require(__dirname + '/../app/app');
 
 // require('../server');
 
-describe('testing ploppability', () => {
-  it('use copies a plop template', (done) => {
-    // console.log(command);
-    expect('use').to.not.eql('Failed to copy template to your project.');
+// describe('testing ploppability', () => {
+//   it('use copies a plop template', (done) => {
+//     // console.log(command);
+//     expect('use').to.not.eql('Failed to copy template to your project.');
+//
+//     done();
+//   });
+//
+// });
 
-    done();
+describe('.action', () => {
+  it('should exist and be a function', () => {
+    should.exist(vorpal.command.action);
+    vorpal.command.shoul.be.type
   });
-
 });
