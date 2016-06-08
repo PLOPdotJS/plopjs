14289d5bf9ac18742afea8d3fc8fc0114051df8c
Stephen Schroeder <schrode50@gmail.com>
2016-06-08 13:19:18 -0700

    new tweak

--- test/test.js
+++ test/test.js
@@ -2,18 +2,12 @@
 
 const chai = require('chai');
 const expect = chai.expect;
-<<<<<<< HEAD
-const should = require('chai').should();
-=======
 
-
-//
->>>>>>> bc3dbdba8d1a9e6d1f9f9b71eb6845bc4e96efde
 const install = require(__dirname + '/../lib/vorpal-install');
 
 describe('install unit test', () => {
   it('should validate the install function of plopjs', () => {
     expect(install).to.be.function;
-    expect(install).to.have.property('template');
+    // expect(install[function]).to.have.property('template');
   });
 });
