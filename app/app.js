fd55f74b6a4d2c6a220b4ae43323e745c9535b1f
Dan Stineback <dstineback@gmail.com>
2016-06-08 10:00:50 -0700

    saved changes from merge

--- app/app.js
+++ app/app.js
@@ -1,12 +1,16 @@
 'use strict';
 
-const fs = require('fs-extra');
 const vorpal = require('vorpal')();
-const GIT = require('nodegit');
 
 vorpal.delimiter('plop$').show();
 
+const use = require(__dirname + '/../lib/vorpal-use');
+const install = require(__dirname + '/../lib/vorpal-install');
+const del = require(__dirname + '/../lib/vorpal-delete');
+const list = require(__dirname + '/../lib/vorpal-list');
+
 vorpal.command('install [template_repo_url] [rename]', 'clones a plop template from github into your ~/.config/plop/ directory')
+
   .action(function(args, callback){
     let template = args.template_repo_url;
     let rename = args.rename;
@@ -30,9 +34,11 @@ vorpal.command('use [template] [path]', 'copies a plop template (by filename) yo
   });
 
 vorpal.command('delete [template]', 'removes a locally saved plop template')
+
 //rimraf or dell
 
-vorpal.command('init [template]', 'walks you through building a plop template')
+vorpal.command('init [template_name]', 'walks you through building a plop template')
 //mkdirp
 
 vorpal.command('list', 'displays a list of templates saved locally')
+  .action(list);
